b33a1e7c0c5360325380c89f06359d111b3395e1
Chris Coleman-Smith <cec24@phy.duke.edu>
2013-05-28 16:17:52 -0400

    working on debugging testing nice plotting functions for the mlegp output

--- R-pkg/R/mlegp-predict-grid.R
+++ R-pkg/R/mlegp-predict-grid.R
@@ -141,47 +141,51 @@ implaus.output.at.point <- function(xpt, fit.pca, obs.means, obs.errs, train.sca
   list(implaus.inde=implaus.inde, implaus.joint=implaus.joint)
 }
 
+## custom functions for plotting the main effects. Should be in their own file for the library
+
 ## plot the main effects in the PCA space
 ## calls plot.decomp.helper
-plot.main.effects.pca <- function(fit.list, nobs=6, file.name.stub="main-effects-"){
+plot.main.effects.pca <- function(fit.list, nobs=5, file.name.stub="main-effects-", ylim.in=c(-1,1)){
+
   ## helper routine to plot main effects in a not horrible way
-  plot.decomp.helper <- function(obs.gp, plot.leg=TRUE){
+  plot.decomp.helper <- function(obs.gp, param.names, plot.leg=TRUE, plot.ylim=c(-1,1)){
     nplots <- obs.gp$numDim
     colvec <- 1:nplots
     dat <- plotMainEffects(obs.gp, no.plot=TRUE)
-    plot(dat$index, dat$preds[1,], type="l", col=colvec[1], ylim=c(-1,1),
+    plot(dat$index, dat$preds[1,], type="l", col=colvec[1], ylim=plot.ylim,
          xlab="param value (arb)", ylab="predicted output (arb)", lwd=2)
     for(i in 2:nplots){
       lines(dat$index, dat$preds[i,], col=colvec[i], lwd=2, lty=i)
     }
     if(plot.leg){
-      legend("bottomright", desNames, col=colvec, lwd=rep(2, nplots), lty=1:nplots)
+      legend("bottomright", param.names, col=colvec, lwd=rep(2, nplots), lty=1:nplots)
     }
     ## return the results
     main.effects <- cbind(dat$index, t(dat$preds))
-    colnames(main.effects) <- c("param.val", desNames)
+    colnames(main.effects) <- c("param.val",param.names )
     invisible(main.effects)
   }
 
   ## try and work sensibly if there's only a single fit instead of a whole gp
-  if(class(fit.pca)=="gp.list"){
-    nobs.max <- (fit.pca$UD)[1]
+  if(class(fit.list)=="gp.list"){
+    nobs.max <- dim(fit.list$UD)[2] ## use the max of the pca dimension
     if(nobs > nobs.max)
       nobs <- nobs.max
     ## try and divide the canvas up nicely
-    par(mfrow=c(2,round(nobs/2)))
+    ncol = min(3, round(nobs)/2+1)
+    par(mfrow=c(2,ncol))
   } else {
     nobs = 1
   }
-    
+
   for(i in 1:nobs){
     if(i == 1){
-      d <- plot.main.custom(fit.list[[i]])
+      d <- plot.decomp.helper(fit.list[[i]], fit.list$params, plot.ylim=ylim.in)
     } else {
-      d <- plot.main.custom(fit.list[[i]], plot.leg=FALSE)
+      d <- plot.decomp.helper(fit.list[[i]], fit.list$params, plot.leg=FALSE, plot.ylim=ylim.in)
     }
     title(main=paste("obs.", i, sep=""))
-    file.name <- paste(file.name.stub,i".dat", sep="")
+    file.name <- paste(file.name.stub,i,".dat", sep="")
     write.table(d, file=file.name, row.names=FALSE)
   }
 }
@@ -197,7 +201,8 @@ plot.main.effects.true <- function(fit.pca, plot.leg=TRUE, ylim=c(-3,3))
     stop("this function is for plotting the main effects of a gp.list\n")
   }
 
-  nobs.max <- (fit.pca$UD)[1]  
+  nobs.true <- (fit.pca$UD)[1] ## how many rotated/true values
+  nparams <- fit.pca$numDim ## how many parameters
   colvec = 1:nobs.max
   
   nobs.gp <- fit.pca$numGPs
@@ -210,12 +215,11 @@ plot.main.effects.true <- function(fit.pca, plot.leg=TRUE, ylim=c(-3,3))
     if(i ==1)
       xs <- effecs$index
   }
-  
-  scaled.mean.vec <- attr(model.scaled, "scaled:center")
-  scaled.scale.vec <- attr(model.scaled, "scaled:scale")
 
-  main.effects <- vector("list", nbins)
-  for(i in 1:nbins) ## entries are observables 1:nbins
+  ## should do the unscaling here, load in the scales from the fit files
+
+  main.effects <- vector("list", nobs.true)
+  for(i in 1:nobs.true) ## entries are observables 1:nobs.true
     main.effects[[i]]$mat <- matrix(0, nrow=length(xs), ncol=nparams) # cols are params
   
   ## generate the reconstructed main effect in each observable, (this now has 6 columns)
@@ -227,15 +231,16 @@ plot.main.effects.true <- function(fit.pca, plot.leg=TRUE, ylim=c(-3,3))
 
     ymain.eff <- t(fit.pca$UD %*% t(dat))
     #main.effects[[i]] <- ymain.eff ## this is a confusing thing to return
-    for(j in 1:nbins)  ## fill in the effect of the i'th parameter for each observable
+    for(j in 1:nobs.true)  ## fill in the effect of the i'th parameter for each observable
       main.effects[[j]]$mat[,i] <- ymain.eff[,j]
   }
 
   ##browser()
 
   ## plot everything
-  par(mfrow=c(2,round(nobs.max/2)))
-  for(i in 1:nbins){
+  ncol <- min(3, 1+round(nobs.max/2)) ## try and guess the number of cols we need
+  par(mfrow=c(2,ncol))
+  for(i in 1:nobs.true){
     ## now make a plot
     for(j in 1:nparams){
       if(j==1){
